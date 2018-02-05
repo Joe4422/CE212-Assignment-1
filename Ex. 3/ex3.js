4b41e3846988ba57c8a7b9a27b3da6db322e4514
Joe4422 <josephdotbrown@gmail.com>
2018-02-05 16:14:09 +0000

    Finished rewrite of ex3.js

--- Ex. 3/ex3.js
+++ Ex. 3/ex3.js
@@ -1,17 +1,16 @@
 typeEnum = {
-	TYPE_NUMBER : 0,
-	TYPE_OPERATOR : 1,
-	TYPE_DECIMAL : 2,
-	TYPE_CLEAR : 3,
-	TYPE_EQUALS : 4
+	NUMBER : 0,
+	OPERATOR : 1,
+	DECIMAL : 2,
+	CLEAR : 3,
+	EQUALS : 4
 }
 stateEnum = {
-	STATE_INT1 : 0,
-	STATE_INT2 : 1,
-	STATE_OUTPUT : 2
+	INT1 : 0,
+	INT2 : 1
 }
 
-var state = stateEnum.STATE_INT1;
+var state = stateEnum.INT1;
 var currentDisplay = "";
 var int1 = "";
 var int2 = "";
@@ -19,24 +18,28 @@ var operator = "";
 var displayOverwrite = false;
 
 function getType(inputValue) {
-	var outVal;
 	if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].indexOf(inputValue) != -1) {
-		outVal = inputTypeEnum.TYPE_NUMBER;
+		return typeEnum.NUMBER;
 	} else if (["+", "-", "*", "/"].indexOf(inputValue) != -1) {
-		outVal = inputTypeEnum.TYPE_OPERATOR;
+		return typeEnum.OPERATOR;
 	} else if (inputValue == ".") {
-		outVal = inputTypeEnum.TYPE_DECIMAL;
+		return typeEnum.DECIMAL;
 	} else if (inputValue == "clear") {
-		outVal = inputTypeEnum.TYPE_CLEAR;
+		return typeEnum.CLEAR;
 	} else if (inputValue == "=") {
-		outVal = inputTypeEnum.TYPE_EQUALS;
+		return typeEnum.EQUALS;
+	} else {
+		return -1;
 	}
-	return outVal;
 }
 
 function displayWrite(number, append) {
 	var textbox = document.getElementById("outBox");
-	if (currentDisplay == "0" && number != ".") currentDisplay = "";
+	if (currentDisplay == "0" && number != ".") displayOverwrite = true;
+	if (number == "." && currentDisplay.indexOf(".") != -1) {
+		displayOverwrite = false;
+		return currentDisplay;
+	}
 	if (append && !displayOverwrite) {
 		if (currentDisplay.length < 8) {
 			currentDisplay += number;
@@ -53,6 +56,73 @@ function displayWrite(number, append) {
 	return currentDisplay;
 }
 
-function nextState(inputValue) {
-
+function calculatorClear() {
+	int1 = "";
+	int2 = "";
+	operator = "";
+	displayWrite("0");
+	state = stateEnum.INT1;
 }
+
+function calculateOutput() {
+	var out = eval("Number(" + int1 + ")" + operator + "Number(" + int2 + ")");
+	displayWrite(out);	
+	return out;
+}
+
+function nextState(inputValue) {
+	var inputType = getType(inputValue);
+	if (state == stateEnum.INT1) {
+		if (inputType == typeEnum.NUMBER) {
+			int1 = displayWrite(inputValue, true);
+		} else if (inputType == typeEnum.OPERATOR) {
+			operator = inputValue;
+			state = stateEnum.INT2;
+			displayOverwrite = true;
+		} else if (inputType == typeEnum.DECIMAL) {
+			int1 = displayWrite(".", true);
+		} else if (inputType == typeEnum.CLEAR) {
+			calculatorClear();
+		}
+	} else if (state == stateEnum.INT2) {
+		if (inputType == typeEnum.NUMBER) {
+			int2 = displayWrite(inputValue, true);
+		} else if (inputType == typeEnum.OPERATOR) {
+			int1 = calculateOutput();
+			operator = inputValue;
+			displayOverwrite = true;
+		} else if (inputType == typeEnum.DECIMAL) {
+			int1 = displayWrite(".", true);
+		} else if (inputType == typeEnum.CLEAR) {
+			if (int2 != "") {
+				displayWrite("0");
+				int2 = "";
+			} else {
+				calculatorClear();
+			}
+		} else if (inputType == typeEnum.EQUALS) {
+			int1 = calculateOutput();
+			state = stateEnum.INT1;
+			operator = "";
+			displayOverwrite = true;
+		}
+	}
+	console.log(int1 + " " + int2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
